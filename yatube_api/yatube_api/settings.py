ff4e457d66fbd13b5373930eb02e098e3d853687
Lev Azaretov <azaretov123@gmail.com>
2025-05-02 23:52:59 +0300

    fix settings

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -1,6 +1,5 @@
-from pathlib import Path
-
 from datetime import timedelta
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -18,8 +17,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api',
     'posts',
+    'api',
 ]
 
 MIDDLEWARE = [
